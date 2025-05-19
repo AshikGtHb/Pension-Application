3cf95b70b5f7745c1089101daa68f9c34a28011a
Ashik Acharya <83348192+AshikGtHb@users.noreply.github.com>
2025-05-19 13:51:15 -0400

    Create PensionAppApplication.java

---
+++ src/main/java/com/ashik/pension/PensionAppApplication.java
@@ -0,0 +1,12 @@
+package com.ashik.pension;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PensionAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PensionAppApplication.class, args);
+    }
+}
+
